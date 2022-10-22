b8a614e76f484a2e37c4922c099ff32506bd9db3
habin <danny9643@naver.com>
2022-10-22 13:45:27 +0900

    first commit

---
+++ src/main/java/com/habin/codinginterview/CodingInterviewInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.habin.codinginterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingInterviewInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingInterviewInJavaApplication.class, args);
+	}
+
+}   
---
+++ src/test/java/com/habin/codinginterview/CodingInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habin.codinginterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
